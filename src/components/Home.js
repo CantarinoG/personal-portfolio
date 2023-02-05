73ce97e2c1a97ada5ade5c719949ab2337444677
CantarinoG <guilhermecantarino1709@gmail.com>
2023-02-05 12:54:51 -0300

    Add Home component

---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+import "../styles/Home.css";
+
+function Home() {
+
+    return <div className="home-component">
+        <div className="language-container">
+            <span>IDIOMA/LANGUAGE:</span>
+            <div className="language-btn-container">
+                <button>
+                PT-BR
+                </button>
+                <button>
+                EN-US
+                </button>
+            </div>
+        </div>
+        <span className="description">Oi, eu sou Guilherme Cantarino.<br/>Desenvolvedor Front-End.</span>
+        <a href="#" className="description">Me conheça
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>arrow-down</title><path d="M11,4H13V16L18.5,10.5L19.92,11.92L12,19.84L4.08,11.92L5.5,10.5L11,16V4Z"/></svg>
+        </a>
+    </div>
+
+}
+
+export default Home;
